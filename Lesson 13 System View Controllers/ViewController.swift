41b56a4fefe4c266e60cee3e88d86f4f547904a0
Иван Деркачев <longrail@yandex.ru>
2019-04-20 13:52:50 +0300

    Initial Commit

---
+++ Lesson 13 System View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson 13 System View Controllers
+//
+//  Created by Иван longrail on 20/04/2019.
+//  Copyright © 2019 Иван longrail. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
